bcaf2b99bc6ff9bd5ee8ec4b81152a811d6c46c2
HeatherH <hherries@yahoo.com>
2022-05-30 12:51:32 -0600

    Added loader image and create user experience with timeout to complete

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 // Listen for Submit
-document
-  .getElementById("loan-form")
-  .addEventListener("submit", calculateResults);
+document.getElementById("loan-form").addEventListener("submit", function(e) {
+  // Hide Results
+  document.getElementById("results").style.display = "none";
+  // Show Loader
+  document.getElementById("loading").style.display = "block";
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 //Calculate Results Function
-function calculateResults(e) {
+function calculateResults() {
   console.log("Calculating...");
   // UI Variables
   const amount = document.getElementById("amount");
@@ -26,14 +33,25 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+
+    // Show results
+    document.getElementById("results").style.display = "block";
+
+    // Hide Loader GIF
+    document.getElementById("loading").style.display = "none";
   } else {
     showError("Please check your numbers.");
   }
-  e.preventDefault();
 }
 
 // Show Error Function
 function showError(error) {
+  // Hide results
+  document.getElementById("results").style.display = "none";
+
+  // Hide Loader GIF
+  document.getElementById("loading").style.display = "none";
+
   // Create a div
   const errorDiv = document.createElement("div");
 
